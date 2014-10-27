924506042895816f3f8a9c63b982bf5a97eb22ec
chrisriesgo <cmriesgo@gmail.com>
2014-10-27 08:04:52 -0500

    Making progress on a better sample app

--- Samples/Android/AndroidAltBeaconLibrary.Sample/MainActivity.cs
+++ Samples/Android/AndroidAltBeaconLibrary.Sample/MainActivity.cs
@@ -1,54 +1,84 @@
 ﻿using System;
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AltBeaconOrg.BoundBeacon;
 using Android.App;
 using Android.Content;
-using Android.Widget;
+
+using Android.Graphics;
 using Android.OS;
 using Android.Util;
-using Android.Graphics;
-
-using AltBeaconOrg.BoundBeacon;
-using System.Linq;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using Android.Widget;
 
 namespace AndroidAltBeaconLibrary.Sample
 {
-	[Activity(Label = "AltBeacon Sample", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity(Label = "AltBeacon Sample", MainLauncher = true, 
+		Theme = "@style/Theme.AltBeacon",
+		Icon = "@drawable/altbeacon")]
 	public class MainActivity : Activity, IDialogInterfaceOnDismissListener, IBeaconConsumer
 	{
-		const string TAG = "MainActivity";
-
 		private readonly MonitorNotifier _monitorNotifier;
 		private readonly RangeNotifier _rangeNotifier;
 
 		AltBeaconOrg.BoundBeacon.Region _tagRegion, _emptyRegion;
 
-		private RelativeLayout _layout;
 		private Button _backgroundButton, _stopButton, _startButton;
-		private EditText _editText;
+		private ListView _list;
 		private BeaconManager _beaconManager;
-		private bool _paused;
-		private long _lineCount = 0;
+		private readonly List<Beacon> _data;
 
 		public MainActivity()
 		{
 			_monitorNotifier = new MonitorNotifier();
 			_rangeNotifier = new RangeNotifier();
+			_data = new List<Beacon>();
 		}
 
 		protected override void OnCreate(Bundle bundle)
 		{
-			Log.WriteLine(LogPriority.Info, TAG, "OnCreate");
 			base.OnCreate(bundle);
 
 			SetContentView(Resource.Layout.MainActivity);
 
-			_layout = FindViewById<RelativeLayout>(Resource.Id.layout);
 			_backgroundButton = FindViewById<Button>(Resource.Id.backgroundButton);
 			_stopButton = FindViewById<Button>(Resource.Id.stopButton);
 			_startButton = FindViewById<Button>(Resource.Id.startButton);
-			_editText = FindViewById<EditText>(Resource.Id.monitoringText);
+			_list = FindViewById<ListView>(Resource.Id.list);
+
+			_list.Adapter = new ListSource((data, position, convertView, parent) => 
+			{
+				var view = convertView;
+				var beacon = data[position];
+
+				if (view == null)
+				{
+					view  = LayoutInflater.Inflate(Resource.Layout.ListItem, parent, false);
+				}
+	
+				view.FindViewById<TextView>(Resource.Id.beaconId).Text = beacon.Id1.ToString().ToUpper();
+				view.FindViewById<TextView>(Resource.Id.beaconDistance).Text = string.Format("{0:N2}m", beacon.Distance);
+
+				if(beacon.Distance <= .5)
+				{
+//					view.SetBackgroundColor(Color.Red);
+				}
+				else if(beacon.Distance > .5 && beacon.Distance <= 10)
+				{
+//					view.SetBackgroundColor(Color.Yellow);
+				}
+				else if(beacon.Distance > 10)
+				{
+//					view.SetBackgroundColor(Color.Blue);
+				}
+				else
+				{
+//					view.SetBackgroundColor(Color.Transparent);
+				}
+
+				return view;
+			});
 
 			VerityBluetooth();
 
@@ -81,7 +111,6 @@ namespace AndroidAltBeaconLibrary.Sample
 		protected override void OnResume()
 		{
 			base.OnResume();
-			_paused = false;
 
 			_backgroundButton.Click += OnBackgroundClick;
 			_stopButton.Click += OnStopClick;
@@ -105,13 +134,12 @@ namespace AndroidAltBeaconLibrary.Sample
 			_beaconManager.StopRangingBeaconsInRegion(_emptyRegion);
 		}
 
-		void OnStartClick(object sender, EventArgs e)
+		async void OnStartClick(object sender, EventArgs e)
 		{
 			_startButton.Enabled = false;
 			_stopButton.Enabled = true;
 
-			_editText.Text = string.Empty;
-			_lineCount = 0;
+			await ClearData();
 
 			_beaconManager.StartMonitoringBeaconsInRegion(_tagRegion);
 			_beaconManager.StartRangingBeaconsInRegion(_tagRegion);
@@ -122,7 +150,6 @@ namespace AndroidAltBeaconLibrary.Sample
 
 		protected override void OnPause()
 		{
-			_paused = true;
 			base.OnPause();
 
 			_backgroundButton.Click -= OnBackgroundClick;
@@ -143,10 +170,12 @@ namespace AndroidAltBeaconLibrary.Sample
 
 		async void RangingBeaconsInRegion(object sender, RangeEventArgs e)
 		{
+			await ClearData();
+
+			var allBeacons = new List<Beacon>();
 			if(e.Beacons.Count > 0)
 			{
 				var beaconNumber = 0;
-				var allBeacons = new List<Beacon>();
 				foreach(var b in e.Beacons)
 				{
 					allBeacons.Add(b);
@@ -157,32 +186,35 @@ namespace AndroidAltBeaconLibrary.Sample
 				orderedBeacons.ForEach(async (firstBeacon) =>
 				{
 					beaconNumber++;
-					if(firstBeacon.Distance <= .5)
-					{
-						// ~immediate
-						await UpdateDisplay("Beacon " + beaconNumber + " of " + orderedBeacons.Count + "\n" + firstBeacon.Id1 + "\nis about " + string.Format("{0:N2}", firstBeacon.Distance) + " meters away.", Color.Red);
-					}
-					else if(firstBeacon.Distance > .5 && firstBeacon.Distance <= 10)
-					{
-						// ~near
-						await UpdateDisplay("Beacon " + beaconNumber + " of " + orderedBeacons.Count + "\n" + firstBeacon.Id1 + "\nis about " + string.Format("{0:N2}", firstBeacon.Distance) + " meters away.", Color.Yellow);
-					}
-					else if(firstBeacon.Distance > 10)
-					{
-						// ~far
-						await UpdateDisplay("Beacon " + beaconNumber + " of " + orderedBeacons.Count + "\n" + firstBeacon.Id1 + "\nis about " + string.Format("{0:N2}", firstBeacon.Distance) + " meters away.", Color.Blue);
-					}
-					else
-					{
-						// ~unknown
-						await UpdateDisplay("I'm not sure how close you are to the " + beaconNumber + " of " + orderedBeacons.Count + " beacon.\n", Color.Transparent);
-					}
+					await UpdateData(firstBeacon);
 				});
 			}
 			else
 			{
-				// ~unknown
-				await UpdateDisplay("I don't see any beacons nearby.", Color.Transparent);
+				// unknown
+				await ClearData();
+			}
+		}
+
+		void RemoveBeaconsNoLongerVisible(List<Beacon> allBeacons)
+		{
+			if(allBeacons == null || allBeacons.Count == 0) return;
+
+			var delete = new List<Beacon>();
+			foreach(var d in _data)
+			{
+				if(allBeacons.All(ab => ab.Id1.ToString() != d.Id1.ToString()))
+				{
+					delete.Add(d);
+				}
+			}
+	
+			_data.RemoveAll(d => delete.Any(del => del.Id1.ToString() == d.Id1.ToString()));
+
+			if(delete.Count > 0)
+			{
+				delete = null;
+				UpdateList();
 			}
 		}
 
@@ -223,15 +255,21 @@ namespace AndroidAltBeaconLibrary.Sample
 
 		private async Task UpdateDisplay(string message, Color color = default(Color))
 		{
-			Console.WriteLine(message);
+		}
+
+		private async Task UpdateData(Beacon beacon)
+		{
 			await Task.Run(() =>
 			{
-				RunOnUiThread(() =>
+				if(_data.All(d => d.Id1.ToString() != beacon.Id1.ToString()))
 				{
-					_lineCount++;
-					_layout.SetBackgroundColor(color);
-					_editText.Append(_lineCount + ": " + message + "\n\n");
-				});
+					RunOnUiThread(() =>
+					{
+						_data.Add(beacon);
+						_data.Sort((x,y) => x.Distance.CompareTo(y.Distance));
+						UpdateList();
+					});
+				}
 			});
 		}
 
@@ -257,6 +295,8 @@ namespace AndroidAltBeaconLibrary.Sample
 
 		public void OnBeaconServiceConnect()
 		{
+			_beaconManager.SetForegroundBetweenScanPeriod(5000); // 5000 milliseconds
+
 			_beaconManager.SetMonitorNotifier(_monitorNotifier); 
 			_beaconManager.SetRangeNotifier(_rangeNotifier);
 
@@ -275,6 +315,66 @@ namespace AndroidAltBeaconLibrary.Sample
 		public void OnDismiss(IDialogInterface dialog)
 		{
 			Finish();
+		}
+
+		private async Task ClearData()
+		{
+			RunOnUiThread(() =>
+			{
+				_data.Clear();
+				((ListSource)_list.Adapter).UpdateList(_data);
+			});
+		}
+
+		private void UpdateList()
+		{
+			RunOnUiThread(() => 
+			{
+				((ListSource)_list.Adapter).UpdateList(_data);
+			});
+		}
+	}
+
+	public class ListSource : BaseAdapter<Beacon>
+	{
+		private List<Beacon> _data;
+		private Func<List<Beacon>, int, Android.Views.View, Android.Views.ViewGroup, Android.Views.View> _getView;
+
+		public ListSource(Func<List<Beacon>, int, Android.Views.View, Android.Views.ViewGroup, Android.Views.View> getView)
+		{
+			_getView = getView;
+			_data = new List<Beacon>();
+		}
+
+		public override long GetItemId(int position)
+		{
+			return position;
+		}
+		public override Android.Views.View GetView(int position, Android.Views.View convertView, Android.Views.ViewGroup parent)
+		{
+			return _getView(_data, position, convertView, parent);
+		}
+
+		public override int Count
+		{
+			get
+			{
+				return _data.Count;
+			}
+		}
+
+		public override Beacon this[int index]
+		{
+			get
+			{
+				return _data[index];
+			}
+		}
+
+		public void UpdateList(List<Beacon> list)
+		{
+			_data = list;
+			NotifyDataSetChanged();				
 		}
 	}
 }
--- Samples/Android/AndroidAltBeaconLibrary.Sample/Resources/Resource.designer.cs
+++ Samples/Android/AndroidAltBeaconLibrary.Sample/Resources/Resource.designer.cs
@@ -41,11 +41,30 @@ namespace AndroidAltBeaconLibrary.Sample
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int custom_theme_color = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int text_theme_color = 2131034113;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int altbeacon = 2130837504;
 			
 			static Drawable()
 			{
@@ -60,26 +79,29 @@ namespace AndroidAltBeaconLibrary.Sample
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int backgroundButton = 2131034115;
+			// aapt resource value: 0x7f070005
+			public const int backgroundButton = 2131165189;
 			
-			// aapt resource value: 0x7f050001
-			public const int layout = 2131034113;
+			// aapt resource value: 0x7f070002
+			public const int beaconDistance = 2131165186;
 			
-			// aapt resource value: 0x7f050004
-			public const int monitoringEventsText = 2131034116;
+			// aapt resource value: 0x7f070001
+			public const int beaconId = 2131165185;
 			
-			// aapt resource value: 0x7f050002
-			public const int monitoringText = 2131034114;
+			// aapt resource value: 0x7f070003
+			public const int layout = 2131165187;
 			
-			// aapt resource value: 0x7f050005
-			public const int startButton = 2131034117;
+			// aapt resource value: 0x7f070004
+			public const int list = 2131165188;
 			
-			// aapt resource value: 0x7f050006
-			public const int stopButton = 2131034118;
+			// aapt resource value: 0x7f070006
+			public const int startButton = 2131165190;
 			
-			// aapt resource value: 0x7f050000
-			public const int textView1 = 2131034112;
+			// aapt resource value: 0x7f070007
+			public const int stopButton = 2131165191;
+			
+			// aapt resource value: 0x7f070000
+			public const int textView1 = 2131165184;
 			
 			static Id()
 			{
@@ -98,7 +120,10 @@ namespace AndroidAltBeaconLibrary.Sample
 			public const int ActivityBackground = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int MainActivity = 2130903041;
+			public const int ListItem = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int MainActivity = 2130903042;
 			
 			static Layout()
 			{
@@ -125,6 +150,28 @@ namespace AndroidAltBeaconLibrary.Sample
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060002
+			public const int CustomActionBar = 2131099650;
+			
+			// aapt resource value: 0x7f060000
+			public const int CustomTheme = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int Theme_AltBeacon = 2131099649;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
