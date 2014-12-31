a6d08a49a933d3d2c4ae0181e5eb14e17761df14
chrisriesgo <cmriesgo@gmail.com>
2014-12-31 08:55:41 -0600

    Moved some things around

--- Samples/Forms/AltBeaconLibrary.Sample/Android/Resources/Resource.designer.cs
+++ Samples/Forms/AltBeaconLibrary.Sample/Android/Resources/Resource.designer.cs
@@ -66,9 +66,6 @@ namespace AltBeaconLibrary.Sample.Droid
 			// aapt resource value: 0x7f020000
 			public const int altbeacon = 2130837504;
 			
-			// aapt resource value: 0x7f020001
-			public const int Icon = 2130837505;
-			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
