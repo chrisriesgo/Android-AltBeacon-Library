e8fbcd3941d623194f97008656d7925d48f4f550
Vu Duc Tuyen <tuyen@naxam.net>
2017-11-25 23:34:00 +0700

    Correct build errors

--- Samples/Native/BeaconReference.Droid/RangingActivity.cs
+++ Samples/Native/BeaconReference.Droid/RangingActivity.cs
@@ -4,78 +4,87 @@ using Org.Altbeacon.Beacon;
 using Android.App;
 using Android.OS;
 using Android.Widget;
+using Android.Content;
 
 namespace BeaconReference.Droid
 {
-	[Activity]
-	public class RangingActivity : Activity, IBeaconConsumer, IRangeNotifier
-	{
-		protected const string TAG = "RangingActivity";
-		BeaconManager beaconManager = null;
+    [Activity]
+    public class RangingActivity : Activity, IBeaconConsumer, IRangeNotifier
+    {
+        protected const string TAG = "RangingActivity";
+        BeaconManager beaconManager = null;
 
-		public RangingActivity()
-		{
-			beaconManager = BeaconManager.GetInstanceForApplication(this);
-		}
+        public RangingActivity()
+        {
+            beaconManager = BeaconManager.GetInstanceForApplication(this);
+        }
 
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
-			
-			Title = "Ranging";
-			SetContentView(Resource.Layout.activity_ranging);
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
-			beaconManager.Bind(this);
-		}
-		
-		protected override void OnDestroy()
-		{
-			base.OnDestroy();
-			beaconManager.Unbind(this);
-		}
-		
-		protected override void OnPause()
-		{
-			base.OnPause();
-			if (beaconManager.IsBound(this)) beaconManager.SetBackgroundMode(true);
-		}
-		
-		protected override void OnResume()
-		{
-			base.OnResume();
-			if (beaconManager.IsBound(this)) beaconManager.SetBackgroundMode(false);
-		}
+            Title = "Ranging";
+            SetContentView(Resource.Layout.activity_ranging);
 
-		public void LogToDisplay(string line)
-		{
-			RunOnUiThread(() =>
-			{
-				var editText = FindViewById<EditText>(Resource.Id.rangingText);
-       	    	editText.Append(line+"\n");
-			});
-		}
+            beaconManager.Bind(this);
+        }
 
-		#region IBeaconConsumer implementation
-		public void OnBeaconServiceConnect()
-		{
-			beaconManager.AddRangeNotifier(this);
-	
-	        try {
-            	beaconManager.StartRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-			} 
-			catch (RemoteException ex) {   
-			}
-		}
-		#endregion
-		
-		#region IRangeNotifier implementation
-		public void DidRangeBeaconsInRegion(ICollection<Beacon> beacons, Region region)
-		{
-			if (beacons.Count > 0) {
-				Beacon firstBeacon = beacons.First();
-				LogToDisplay("The first beacon " + firstBeacon.ToString() + " is about " + firstBeacon.Distance + " meters away.");
-			}
-		}
-		#endregion
-	}
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            beaconManager.Unbind(this);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            if (beaconManager.IsBound(this)) beaconManager.BackgroundMode = (true);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (beaconManager.IsBound(this)) beaconManager.BackgroundMode = (false);
+        }
+
+        public void LogToDisplay(string line)
+        {
+            RunOnUiThread(() =>
+            {
+                var editText = FindViewById<EditText>(Resource.Id.rangingText);
+                editText.Append(line + "\n");
+            });
+        }
+
+        #region IBeaconConsumer implementation
+        public void OnBeaconServiceConnect()
+        {
+            beaconManager.AddRangeNotifier(this);
+
+            try
+            {
+                beaconManager.StartRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+            }
+            catch (RemoteException ex)
+            {
+            }
+        }
+        #endregion
+
+        #region IRangeNotifier implementation
+        public void DidRangeBeaconsInRegion(ICollection<Beacon> beacons, Region region)
+        {
+            if (beacons.Count > 0)
+            {
+                Beacon firstBeacon = beacons.First();
+                LogToDisplay("The first beacon " + firstBeacon.ToString() + " is about " + firstBeacon.Distance + " meters away.");
+            }
+        }
+
+        public bool BindService(Intent p0, IServiceConnection p1, int p2)
+        {
+            return true;
+        }
+        #endregion
+    }
 }
